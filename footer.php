8537cfc37423fe94110ba7db80da10023a67c8f1
fiebryhoga <dimasfiebry@gmail.com.com>
2024-06-04 19:15:37 +0700

    mvc-mysql

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="w-screen fixed bg-gray-200 bottom-0 flex justify-center items-center py-4">
-    <p class="font-medium text-gray-400 text-xs">Case Chat Kelompok 1 - PWTI 2024</p>
-</footer>
